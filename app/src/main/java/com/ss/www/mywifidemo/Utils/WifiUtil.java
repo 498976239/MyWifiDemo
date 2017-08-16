55fdc5e3915d9db80fe8a9f07c89c8bff7778efc
yangxuesong <498976239@qq.com>
2017-08-16 15:23:10 +0800

    1

--- app/src/main/java/com/ss/www/mywifidemo/Utils/WifiUtil.java
+++ app/src/main/java/com/ss/www/mywifidemo/Utils/WifiUtil.java
@@ -1,8 +1,17 @@
 package com.ss.www.mywifidemo.Utils;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
+
 /**
  * Created by Administrator on 2017/8/14.
  */
 
 public class WifiUtil {
+    private Context mContext;
+    private WifiManager mWifiManager;
+    public WifiUtil(Context context){
+        this.mContext = context;
+        mWifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+    }
 }
